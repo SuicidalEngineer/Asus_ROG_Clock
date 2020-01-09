afc9f5a12915980d6585b35212fedf60e7f23220
Efecan Yılmazdemir <59260806+SuicidalEngineer@users.noreply.github.com>
2020-01-10 01:19:06 +0300

    Update Asus_ROG_clock.ino

--- Asus_ROG_clock/Asus_ROG_clock.ino
+++ Asus_ROG_clock/Asus_ROG_clock.ino
@@ -8,17 +8,17 @@ DS3231  rtc(SDA, SCL);
 
 dht11 DHT11;
 
- int redPin = 11;   // Red LED,  connected to digital pin 11  
- int grnPin = 10;  // Green LED, connected to digital pin 10  
- int bluPin = 9;  // Blue LED, connected to digital pin 9  
+ int redPin = 11; 
+ int grnPin = 10; 
+ int bluPin = 9;
   
  void setup(){     
     
-  Serial.begin(9600);   // Opening serial connection at 9600bps   
-  Serial.println("Humidity and temperature");   // Printing Serial Monitor header  
-  delay(1000);   // Wait 1 second before printing data to Serial Monitor  
+  Serial.begin(9600); 
+  Serial.println("Temperature");
+  delay(1000);   // Printing Serial Monitor header    
     
-  pinMode(redPin, OUTPUT);   // Sets the pins as output for RGB LED  
+  pinMode(redPin, OUTPUT);    // Printing Serial Monitor header    
   pinMode(grnPin, OUTPUT);  
   pinMode(bluPin, OUTPUT);  
   rtc.begin();
@@ -44,10 +44,7 @@ dht11 DHT11;
         
    Serial.print("Sicaklik (Celcius): ");
    Serial.println((float)DHT11.temperature);
- 
-    
-  // Setting the colours of the RGB LED based on temperature,  
- // Colour is shown for 0.1s before checking temperature again.    
+
    
   if((DHT11.temperature < 50) && (DHT11.temperature >= 40))  
   {  
@@ -58,30 +55,30 @@ dht11 DHT11;
 
   if((DHT11.temperature < 40) && (DHT11.temperature >= 30)) 
   {
-   digitalWrite(grnPin, HIGH);  // YELLOW  
+   digitalWrite(grnPin, HIGH);  
    digitalWrite(redPin, HIGH);
    digitalWrite(bluPin, LOW);  
   }
   if((DHT11.temperature < 30) && (DHT11.temperature >= 20)) 
   {  
-   digitalWrite(grnPin, HIGH);  // GREEN
+   digitalWrite(grnPin, HIGH);
    digitalWrite(redPin, LOW); 
    digitalWrite(bluPin, LOW);     
   }
   
   if((DHT11.temperature >= 10) && (DHT11.temperature < 20))  
   {  
-   digitalWrite(grnPin, HIGH);  // aqua  
+   digitalWrite(grnPin, HIGH); 
    digitalWrite(bluPin, HIGH); 
    digitalWrite(redPin, LOW);   
   }  
   
   if((DHT11.temperature < 10) && (DHT11.temperature >= 0))  
   {  
-   digitalWrite(grnPin, HIGH);  // WHITE  
+   digitalWrite(grnPin, HIGH);  
    digitalWrite(bluPin, HIGH);  
    digitalWrite(redPin, HIGH);    
   }  
 
-  delay(1000);   // Sensor shouldn't be read too frequently so delay of 1s  
+  delay(1000);  
  }
